24411f87b469ecdc791fc3d096f3b45c608f5749
Khalid Ansari <khaliljpc@gmail.com>
2024-07-11 11:19:25 +0530

    feat: added main page content

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   GitHubLogoIcon,
   HamburgerMenuIcon,
@@ -5,10 +7,17 @@ import {
   TwitterLogoIcon,
 } from '@radix-ui/react-icons';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsMenuOpen(prev => !prev);
+  };
+
   return (
-    <header className="md:px-3 px-7 left-1/2 -translate-x-24 md:-translate-x-0 py-3 md:py-7 fixed md:top-1/2 top-4  md:left-4 md:-translate-y-10 z-10  border border-zinc-400 border-opacity-30 bg-zinc-900 md:w-fit rounded-3xl">
+    <header className="md:px-3 px-7  left-1/2 -translate-x-24 md:-translate-x-0 py-3 md:py-7 fixed md:top-1/2 top-4   md:left-4 md:-translate-y-20 z-10  border border-zinc-400 border-opacity-30 bg-zinc-900 md:w-fit rounded-3xl">
       <nav className="flex flex-row md:flex-col gap-6">
         <Link href="https://x.com/stan8783">
           <TwitterLogoIcon className="text-zinc-100 w-5 h-5" />
@@ -19,8 +28,12 @@ const Navbar = () => {
         <Link href="https://www.linkedin.com/in/khalid-ansari-917357230/">
           <LinkedInLogoIcon className="text-zinc-100 w-5 h-5" />
         </Link>
-        <HamburgerMenuIcon className="text-zinc-100 w-5 h-5 md:hidden block" />
+        <HamburgerMenuIcon
+          onClick={handleClick}
+          className="text-zinc-100 w-5 h-5 md:hidden block"
+        />
       </nav>
+      {/* <div className="bg-white text-blue-400" >kjdn</div> */}
     </header>
   );
 };
